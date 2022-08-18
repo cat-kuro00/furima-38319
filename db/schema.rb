87210b6a0a433ac4f8690831fd83b0ea379a8d90
cat-kuro00 <kendo19980626@icloud.com>
2022-08-18 15:00:08 +0900

    newアクション・マイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_12_084114) do
+ActiveRecord::Schema.define(version: 2022_08_18_052654) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item", null: false
+    t.text "info", null: false
+    t.integer "category", null: false
+    t.integer "condition", null: false
+    t.integer "shipping", null: false
+    t.integer "prefecture", null: false
+    t.integer "ship_days", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 2022_08_12_084114) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
