34927ec382c98beeaecb47053767f4464e1ef5cc
cat-kuro00 <kendo19980626@icloud.com>
2022-08-19 21:28:01 +0900

    手数料・利益計算

---
+++ app/javascript/item_price.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function(){
+
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const Tax = Math.floor(inputValue * 0.1);
+    const addBenefitDom = document.getElementById("profit");
+    
+    addTaxDom.innerHTML = Tax
+    addBenefitDom.innerHTML = inputValue - Tax
+  })
+})
