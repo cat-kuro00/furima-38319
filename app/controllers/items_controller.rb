daccd61fc64dce2040199431a834b14e76b25efd
cat-kuro00 <kendo19980626@icloud.com>
2022-08-24 16:55:08 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :edit]
-  before_action :move_to_index, only: [:edit]
+  before_action :authenticate_user!, except: [:index, :show]
+  
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -47,10 +47,6 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :info, :category_id, :condition_id, :shipping_id, :prefecture_id, :ship_days_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+  
   
 end
