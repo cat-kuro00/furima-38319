896d60724df03809d83b1b5081389510499d9cbb
cat-kuro00 <kendo19980626@icloud.com>
2022-08-29 15:42:10 +0900

    購入ボタンok

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,8 @@
 class OrdersController < ApplicationController
+   before_action :set, only: [:index, :create]
+
   def index
-    
-    @item = Item.find_by(id: params[:item_id])
-    
-
-    @order = BuyAddress.new
-  end
-
-  def new
-    @buy_address = BuynAddress.new
+    @buy_address = BuyAddress.new
   end
 
   def create
@@ -16,21 +10,27 @@ class OrdersController < ApplicationController
     if @buy_address.valid?
       Payjp.api_key = "sk_test_35604bbb7014a1376345c4bf"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
       Payjp::Charge.create(
-        amount: order_params[:price],  # 商品の値段
+        amount: @item.price,  # 商品の値段
         card: order_params[:token],    # カードトークン
         currency: 'jpy'                 # 通貨の種類（日本円）
       )
       @buy_address.save
+      
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:buy_address).permit(:post, :prefecture_id, :city, :block_num, :building, :price, :phone_num, :buy_id).merge(token: params[:token], user_id: current_user.id)
+    params.require(:buy_address).permit(:post, :prefecture_id, :city, :block_num, :building, :phone_num).merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
   end
+
+  def set
+    @item = Item.find_by(id: params[:item_id])
+  end
+
 
 end
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,23 +1,25 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :token, :post, :prefecture_id, :city, :block_num, :building, :price, :phone_num, :buy_id, :user_id
+  attr_accessor :token, :post, :prefecture_id, :city, :block_num, :building, :phone_num, :buy_id, :user_id, :item_id
+
 
 
   with_options presence: true do
     validates :token
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
+    # validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
     validates :user_id
     validates :post, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :block_num
     validates :phone_num
-    validates :buy_id
+    # validates :buy_id
+    validates :item_id
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
   
 
   def save
-    buy = Buy.create(buy_id: buy_id, user_id: user_id)
-    Address.create(post: post, prefecture_id: prefecture_id, city: city, block_num: block_num, building: building, buy_id: buy.id)
+    Buy.create(item_id: item_id, user_id: user_id)
+    Address.create(post: post, prefecture_id: prefecture_id, city: city, block_num: block_num, building: building, buy_id: buy_id)
   end
 end
